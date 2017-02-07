e122c04c390f656be1289eb78d1e26cc5f043ae4
Michael Zelenyuk <zell.xp@gmail.com>
2017-02-07 10:41:36 +0200

    Add Room model

---
+++ db/migrate/20170207083637_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[5.0]
+  def change
+    create_table :rooms do |t|
+      t.string :title, null: false
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
