e7bc78a72eb0a7f7c7cba6753adf220194af3115
Michael Zelenyuk <zell.xp@gmail.com>
2017-02-24 11:01:10 +0200

    Create 'coverage' directory on CircleCI after build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ require 'simplecov'
 # Save results to CircleCI artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
+  SimpleCov.coverage_dir(dir && 'coverage')
 end
 
 SimpleCov.start
