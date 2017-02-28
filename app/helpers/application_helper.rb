7c436fc68a5096b949e972a7549567a2fa59c103
Michael Zelenyuk <zell.xp@gmail.com>
2017-02-28 13:26:38 +0200

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}",
               opts)
   end
+
+  def bootstrap_class_for(flash_type)
+    types = {success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info'}
+    types.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(helper.gravatar_for(user)).to match("img alt=\"#{user.name}\" src=\"https://www.gravatar.com/avatar/")
     end
   end
+
+  describe '#bootstrap_class_for' do
+    it 'returns the correct success type' do
+      expect(helper.bootstrap_class_for('success')).to eq('alert-success')
+    end
+
+    it 'returns the correct error type' do
+      expect(helper.bootstrap_class_for('error')).to eq('alert-danger')
+    end
+
+    it 'returns the correct alert type' do
+      expect(helper.bootstrap_class_for('alert')).to eq('alert-warning')
+    end
+
+    it 'returns the correct notice type' do
+      expect(helper.bootstrap_class_for('notice')).to eq('alert-info')
+    end
+  end
 end
