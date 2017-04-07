b355d05b9cd69a0c3fc0c1513f3515e277576dcf
Michael Zelenyuk <zell.xp@gmail.com>
2017-04-07 14:48:51 +0300

    Add users list to Room header

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,4 +6,8 @@ $(document).ready(function () {
 
 $(document).on('turbolinks:load', function () {
   $('[data-toggle="popover"]').popover();
+
+  $(".show-users-list").on("click", function () {
+    $(this).find($(".glyphicon")).toggleClass('glyphicon-menu-down').toggleClass('glyphicon-menu-up');
+  });
 });
